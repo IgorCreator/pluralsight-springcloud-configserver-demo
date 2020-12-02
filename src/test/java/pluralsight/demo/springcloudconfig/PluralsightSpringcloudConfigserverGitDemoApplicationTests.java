f35f899f978e29c8f2a2849ea46e81b4654133c6
Igor Asamadzinau <igor.asamadzinau@fmr.com>
2020-12-01 20:42:00 -0500

    Initial commit

---
+++ src/test/java/pluralsight/demo/springcloudconfig/PluralsightSpringcloudConfigserverGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pluralsight.demo.springcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightSpringcloudConfigserverGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
